81a85469217c31dda1f8ea5f863d186b5e5fe043
DJILI Mohamed Amine <aminedjili29@gmail.com>
2025-05-16 16:48:05 +0100

    Modifié le script check_balance.js pour corriger l'initialisation de Web3 et afficher le solde du contrat

--- check_balance.js
+++ check_balance.js
@@ -1,14 +1,12 @@
 const { Web3 } = require('web3');
-
 const web3 = new Web3('https://ethereum-sepolia-rpc.publicnode.com');
+const ABI = require('./ABI.json');
 
 // Pour TEST
 const contractAddress = '0xed1129a977d2ee91D0CF9689e2bd17664fB3d89F'; 
+const contract = new web3.eth.Contract(ABI, contractAddress);
 
-async function readBalance() {
-  const balance = await web3.eth.getBalance(contractAddress);
-  console.log(`Solde du contrat (en wei) : ${balance}`);
-  console.log(`Solde du contrat (en ETH) : ${web3.utils.fromWei(balance, 'ether')}`);
+const getBalance = async() => {
+  const balance = await contract.methods.balance().call();
+  console.log('Le solde est => ' + balance);
 }
-
-readBalance();
