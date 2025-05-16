5e9a3d6f0bf2a505202217c4c71dae69fd8bb057
DJILI Mohamed Amine <aminedjili29@gmail.com>
2025-05-16 16:50:13 +0100

    Changer l'adresse du contrat

--- check_balance.js
+++ check_balance.js
@@ -3,7 +3,7 @@ const web3 = new Web3('https://ethereum-sepolia-rpc.publicnode.com');
 const ABI = require('./ABI.json');
 
 // Pour TEST
-const contractAddress = '0xed1129a977d2ee91D0CF9689e2bd17664fB3d89F'; 
+const contractAddress = '0x9F68e2c12E7350Fab814cFf12f1Ef438482e46C5'; 
 const contract = new web3.eth.Contract(ABI, contractAddress);
 
 const getBalance = async() => {
